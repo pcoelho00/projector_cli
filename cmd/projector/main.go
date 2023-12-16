fb19074101f207eb921d51ff89c27fb4242c23e2
pcoelho00 <pcoelho00@outlook.com>
2023-12-16 19:58:08 -0300

    first

---
+++ cmd/projector/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/pcoelho00/projector_cli/pkg/projector"
+)
+
+func main() {
+	opts, err := projector.GetOpts()
+	if err != nil {
+		log.Fatalf("unable to get options %v", err)
+	}
+
+	config, err := projector.NewConfig(opts)
+	if err != nil {
+		log.Fatalf("unable to get config %v", err)
+	}
+
+	// fmt.Printf("opts: %+v", opts)
+	fmt.Printf("opts: %+v", config)
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pcoelho00/projector_cli
+
+go 1.21.3
+
+require github.com/hellflame/argparse v1.12.0
